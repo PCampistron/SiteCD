be485690a7cf8e2fac0250b7cf67c7386d03e0f0
Pierre Campistron <pier.campistron@gmail.com>
2022-12-08 12:21:08 +0100

    Affichage des CD

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-    echo "Hello, world!";
+    $dbname='dsokhna_bd'; // Nom de la base de données
+    $dsn="mysql:host=lakartxela;dbname=$dbname";
+    $user ='dsokhna_bd';
+    $pass='dsokhna_bd';
+
+    $size_output = 100;
+
+
+    $db = new PDO($dsn, $user, $pass);
+
+    $requete = $db->prepare("SELECT titre, auteur, prix, lienImage FROM CD ORDER BY titre");
+
+    $requete->execute();
+    $resultat=$requete->fetchAll();
+
+    foreach($resultat AS $row)
+    {
+        $text = $row['titre'] . " - " . $row['auteur'] . " - " . $row['prix'] . "<BR>";
+
+        $url = $row['lienImage'];
+
+        echo "<img src='$url" . "R.jpg'>";
+
+        echo $text;
+    }
+
 ?>
