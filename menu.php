3273b8fc20e4cc085dbfc985f39a01c7b3f784a5
dsokhna <daddydjim20@gmail.com>
2023-01-04 09:02:46 +0100

    Insertion des images restantes(Gazo et Hallyday)

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
     echo "<nav>";
 
     echo "<a href ='index.php'> Accueil </a>";
-    echo "<a href='panier.php'> Panier </a>";
+    echo "<a href ='panier.php'> Panier </a>";
 
     echo "</nav>";
 
